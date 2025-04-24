6fc1a6252af609000752f470f4089a906dc476f4
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-04-24 23:49:39 +0900

    名前を名乗る

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('#button');
 button.addEventListener("click",function(){
+    const name = document.querySelector("name").value;
+    const message = 'こんにちは、私の名前は${name}です。';
     const output = document.querySelector("#output");
-    output.textContent = "こんにちは";
+    output.textContent = message;
 });
