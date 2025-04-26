7db910cc73f91ec77e294879ceea6a622f863811
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-04-26 23:08:07 +0900

    キーワードを配列に入れる

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const interests = ["ゲーム","アニメ","プログラミング"];
+const specialities = ["寝ること","食べること","運動すること"];
+const greetings = ["よろしくね","お手柔らかに","押忍"];
 const button = document.querySelector('#button');
 button.addEventListener("click",function(){
     const name = document.querySelector("#name").value;
