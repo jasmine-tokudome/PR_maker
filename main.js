2bb02092d4fc3382ecd27e4ff6a3b9b3cda82e69
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-04-26 23:17:12 +0900

    ランダムなキーワードの出力

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const greetings = ["よろしくね","お手柔らかに","押忍"];
 const button = document.querySelector('#button');
 button.addEventListener("click",function(){
     const name = document.querySelector("#name").value;
-    const message = `こんにちは、私の名前は${name}です。`;
+    const index1 = Math.floor(Math.random() * interests.length);
+    const index2 = Math.floor(Math.random() * specialties.length);
+    const index3 = Math.floor(Math.random() * greetings.length);
+    const interest = interests[index1];
+    const specialties = specialties[index2];
+    const greeting = greetings[index3]
+    const message = `
+    こんにちは、私の名前は${name}です。
+    最近はまっているのは${interest}で、
+    特技は${specialty}です。
+    ${greeting}`;
     const output = document.querySelector("#output");
     output.textContent = message;
 });
