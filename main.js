77bb9f963e01e2b7990006ea335b8c4f121084f2
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-08-14 00:31:47 +0900

    ランダムにキーワードを取り出す

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const greetings = ["よろしくね","お手柔らかに","押忍！"]
 const button = document.querySelector("#button");
 button.addEventListener("click", function(){
     const name = document.querySelector("#name").value;
+    const index1 = Math.floor(Math.random() * interests.length);
+    const index2 = Math.floor(Math.random() * specialities.length);
+    const index3 = Math.floor(Math.random() * greetings.length);
+    const interest = interests[index1];
+    const speciality = interests[index2];
+    const greeting = interests[index3];
     const message = `こんにちは、私の名前は${name}です。`
     const output = document.querySelector("#output");
     output.textContent = message;
