9f21eb9c14ec585db61818c8c96f437209f3daf1
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-08-23 21:35:44 +0900

    レポジトリを分けるために整理

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const interests = ["明るい","優しい","力強い","魅力的な","前向きな"]
-const specialities = ["笑顔","努力","思いやり","言葉","センス"]
-const greetings = ["幸せ","安心","元気","勇気","笑顔"]
+const interests = ["ゲーム","アニメ","プログラミング"]
+const specialities = ["寝ること","食べること","運動すること"]
+const greetings = ["よろしくね","お手柔らかに","押忍！"]
 const button = document.querySelector("#button");
 button.addEventListener("click", function(){
     const name = document.querySelector("#name").value;
@@ -8,12 +8,9 @@ button.addEventListener("click", function(){
     const index2 = Math.floor(Math.random() * specialities.length);
     const index3 = Math.floor(Math.random() * greetings.length);
     const interest = interests[index1];
-    const speciality = specialities[index2];
-    const greeting = greetings[index3];
-    const message = `{name}さんは、
-    本当に {A} な人で、
-    {B} によって周りを {C} にしています。
-    `;
+    const speciality = interests[index2];
+    const greeting = interests[index3];
+    const message = `こんにちは、私の名前は${name}です。`
     const output = document.querySelector("#output");
     output.textContent = message;
 });
